6b1bb37b2d11383e8738fbd9d6cfe541810189d5
dan_kucherenko <daniil.ukraine@gmail.com>
2023-03-17 01:40:45 +0200

    Additional files added

---
+++ Lab02Kucherenko/Lab02Kucherenko/Tools/SunSign.cs
@@ -0,0 +1,18 @@
+﻿namespace KMA.Lab02.Kucherenko.Tools
+{
+    internal enum SunSign
+    {
+        Aries,
+        Taurus,
+        Gemini,
+        Cancer,
+        Leo,
+        Virgo,
+        Libra,
+        Scorpio,
+        Sagittarius,
+        Capricorn,
+        Aquarius,
+        Pisces
+    }
+}
