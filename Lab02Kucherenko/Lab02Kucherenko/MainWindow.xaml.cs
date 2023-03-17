903e3a556457986d00386f60a304455966a3aa54
dan_kucherenko <daniil.ukraine@gmail.com>
2023-03-17 13:00:59 +0200

    Add initial views

---
+++ Lab02Kucherenko/Lab02Kucherenko/MainWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace KMA.Lab02.Kucherenko
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Lab02Kucherenko/Lab02Kucherenko/Views/FormView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using KMA.Lab02.Kucherenko.ViewModels;
+using System.Windows.Controls;
+
+namespace KMA.Lab02.Kucherenko.Views
+{
+    /// <summary>
+    /// Interaction logic for FormView.xaml
+    /// </summary>
+    public partial class FormView : UserControl
+    {
+        private FormViewModel _viewModel;
+
+        public FormView()
+        {
+            InitializeComponent();
+            DataContext = _viewModel = new FormViewModel();
+        }
+    }
+}
